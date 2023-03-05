96ed4a73c114badc2b83fa26abc85830b8289b72
Xenophon Kitsios <xenophon.kitsios@outlook.com>
2023-03-05 02:21:34 +0200

    Ούτε σήμερα δεν δούλεψε

--- sim_piece_demo.py
+++ sim_piece_demo.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
 from iotdb.Session import Session
+from iotdb.template.InternalNode import InternalNode
+from iotdb.template.MeasurementNode import MeasurementNode
+from iotdb.template.Template import Template
 from iotdb.utils.IoTDBConstants import TSDataType, TSEncoding, Compressor
 from iotdb.utils.NumpyTablet import NumpyTablet
 
@@ -14,73 +17,54 @@ def connect():
     return session
 
 
-# def parse_file(file):
-#     df = pd.read_csv(file, header=None)
-#     data_types = [TSDataType.FLOAT]
-#     np_values = [df[1].to_numpy(dtype=TSDataType.FLOAT.np_dtype())]
-#     np_timestamps = df[0].to_numpy(dtype=TSDataType.FLOAT.np_dtype())
-#     measurements = ["value"]
-#
-#     return data_types, np_values, np_timestamps, measurements
+def parse_file(file):
+    df = pd.read_csv(file, header=None)
+    data_types = [TSDataType.FLOAT]
+    np_values = [df[1].to_numpy(dtype=TSDataType.FLOAT.np_dtype())]
+    np_timestamps = df[0].to_numpy(dtype=TSDataType.FLOAT.np_dtype())
+
+    return data_types, np_values, np_timestamps
 
 
-# def chimp(session, measurements, data_types, np_values, np_timestamps):
-#     if session.check_time_series_exists("root.demo.chimp"):
-#         session.delete_time_series(["root.demo.chimp"])
-#     session.create_time_series(
-#         "root.demo.chimp", TSDataType.FLOAT, TSEncoding.CHIMP, Compressor.UNCOMPRESSED
-#     )
-#     np_tablet = NumpyTablet(
-#         "root.demo.chimp.ts1", measurements, data_types, np_values, np_timestamps
-#     )
-#     session.insert_tablet(np_tablet)
-
-
-# def sim_piece(session, measurements, data_types, np_values, np_timestamps):
-#     if session.check_time_series_exists("root.demo.sim_piece"):
-#         session.delete_time_series(["root.demo.sim_piece"])
-#     session.create_time_series(
-#         "root.demo.sim_piece", TSDataType.FLOAT, TSEncoding.SIM_PIECE, Compressor.UNCOMPRESSED
-#     )
-#     np_tablet = NumpyTablet(
-#         "root.demo.sim_piece.ts1", measurements, data_types, np_values, np_timestamps
-#     )
-#     session.insert_tablet(np_tablet)
-#
-#     session.insert_tablet(np_tablet)
+def add_timeseries(session, node, measurements, data_types, np_values, np_timestamps):
+    np_tablet = NumpyTablet(
+        "root.demo." + node, measurements, data_types, np_values, np_timestamps
+    )
+    session.insert_tablet(np_tablet)
 
 
 def main():
     session = connect()
     session.open(False)
 
-    df = pd.read_csv("dataset/WindDirection.csv.gz", header=None)
+    data_types, np_values, np_timestamps = parse_file("dataset/WindDirection.csv.gz")
 
+    demo_template = Template(name='demo_template')
+
+    demo_node = InternalNode(name="demo", share_time=False)
+    sim_piece_node = InternalNode(name="sim_piece", share_time=True)
+    chimp_node = InternalNode(name="chimp", share_time=True)
+    demo_node.add_child(sim_piece_node)
+    demo_node.add_child(chimp_node)
+
+    sim_piece_measurement_node = MeasurementNode("wind_sim_piece", TSDataType.FLOAT, TSEncoding.SIM_PIECE, Compressor.UNCOMPRESSED)
+    sim_piece_node.add_child(sim_piece_measurement_node)
+
+    chimp_measurement_node = MeasurementNode("wind_chimp", TSDataType.FLOAT, TSEncoding.CHIMP, Compressor.UNCOMPRESSED)
+    chimp_node.add_child(chimp_measurement_node)
+
+    demo_template.add_template(demo_node)
+    demo_template.add_template(sim_piece_node)
+    demo_template.add_template(chimp_node)
+    demo_template.add_template(sim_piece_measurement_node)
+    demo_template.add_template(chimp_measurement_node)
+
+    session.create_schema_template(demo_template)
     session.set_storage_group("root.demo")
+    session.set_schema_template('demo_template', 'root.demo')
 
-    if session.check_time_series_exists("root.demo.sim_piece"):
-        session.delete_time_series(["root.demo.sim_piece"])
-    session.create_time_series("root.demo.sim_piece", TSDataType.FLOAT, TSEncoding.SIM_PIECE, Compressor.UNCOMPRESSED)
-
-    if session.check_time_series_exists("root.demo.chimp"):
-        session.delete_time_series(["root.demo.chimp"])
-    session.create_time_series("root.demo.chimp", TSDataType.FLOAT, TSEncoding.SIM_PIECE, Compressor.UNCOMPRESSED)
-
-    session.insert_records_of_one_device(
-        "root.demo.sim_piece",
-        list(df[0]),
-        [["t" + str(t) for t in df[0]]],
-        [[TSDataType.FLOAT for _ in range(df.shape[0])]],
-        [list(df[1])]
-    )
-
-    session.insert_records_of_one_device(
-        "root.demo.chimp",
-        list(df[0]),
-        [["t" + str(t) for t in df[0]]],
-        [[TSDataType.FLOAT for _ in range(df.shape[0])]],
-        [list(df[1])]
-    )
+    add_timeseries(session, "sim_piece", ["wind_sim_piece"], data_types, np_values, np_timestamps)
+    add_timeseries(session, "chimp", ["wind_chimp"], data_types, np_values, np_timestamps)
 
     # session.delete_storage_group("root.demo")
 
